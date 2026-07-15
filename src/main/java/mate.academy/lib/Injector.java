f72964545d8953763cbefe39777f2a48b285d256
Anastasiia Volichenko <anastasiia.volichenko@gmail.com>
2026-07-15 15:07:18 +0100

    solution number one

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,13 +1,59 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
+
+    private final Map<Class<?>, Class<?>> interfacesImpl = Map.of(
+            ProductService.class, ProductServiceImpl.class,
+            ProductParser.class, ProductParserImpl.class,
+            FileReaderService.class, FileReaderServiceImpl.class);
 
     public static Injector getInjector() {
         return injector;
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> implementationClass = interfacesImpl.get(interfaceClazz);
+
+        if (implementationClass == null) {
+            throw new RuntimeException(
+                    "No implementation class found for " + interfaceClazz.getName());
+        }
+
+        if (!implementationClass.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException(
+                    "No @Component annotation found for " + interfaceClazz.getName());
+        }
+
+        try {
+            Object instance = implementationClass.getDeclaredConstructor().newInstance();
+
+            Field[] fields = implementationClass.getDeclaredFields();
+
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    Object dependency = getInstance(field.getType());
+
+                    field.setAccessible(true);
+                    field.set(instance, dependency);
+                }
+            }
+
+            return instance;
+
+        } catch (InstantiationException | IllegalAccessException
+                 | NoSuchMethodException | InvocationTargetException e) {
+            throw new RuntimeException("Unable to instantiate " + implementationClass.getName(), e);
+        }
     }
 }
